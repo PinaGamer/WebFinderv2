015f1edff5fecf4af7954723e75b001f70bac473
pinagamer <carlospinamartinezdm@gmail.com>
2015-04-20 00:42:31 +0200

    Subida del proyecto a GitHub

---
+++ RastreadorHTML/src/Rastreador/PruebaLector.java
@@ -0,0 +1,17 @@
+package Rastreador;
+
+import java.io.IOException;
+
+
+public class PruebaLector {
+	public static void main(String[]args) throws IOException{
+		
+		LectorHTTP n1 = new LectorHTTP();
+		int i;
+		//n1.mostrarContenido("www.as.com.txt");
+//		for(i=0;i<5;i++){
+//			String [] pagina = n1.crearDireccionWeb();
+//			n1.getContenidoHTML(pagina);
+//		}
+	}
+}
